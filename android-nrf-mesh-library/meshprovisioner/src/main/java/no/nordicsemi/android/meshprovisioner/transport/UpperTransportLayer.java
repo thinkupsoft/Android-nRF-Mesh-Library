b6790dab142d3fd2ea6018c5f88628627fd15896
Nicolas Castro <nicolas.castro@thinkupsoft.com>
2020-01-15 13:10:24 -0300

    change bytes limit to segment

--- android-nrf-mesh-library/meshprovisioner/src/main/java/no/nordicsemi/android/meshprovisioner/transport/UpperTransportLayer.java
+++ android-nrf-mesh-library/meshprovisioner/src/main/java/no/nordicsemi/android/meshprovisioner/transport/UpperTransportLayer.java
@@ -47,7 +47,7 @@ import no.nordicsemi.android.meshprovisioner.utils.SecureUtils;
 abstract class UpperTransportLayer extends AccessLayer {
     private static final String TAG = UpperTransportLayer.class.getSimpleName();
     private static final int PROXY_CONFIG_OPCODE_LENGTH = 1;
-    static final int MAX_SEGMENTED_ACCESS_PAYLOAD_LENGTH = 12;
+    static final int MAX_SEGMENTED_ACCESS_PAYLOAD_LENGTH = 17;
     static final int MAX_UNSEGMENTED_CONTROL_PAYLOAD_LENGTH = 11;
     static final int MAX_SEGMENTED_CONTROL_PAYLOAD_LENGTH = 8;
 
